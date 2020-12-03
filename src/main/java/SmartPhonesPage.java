580e0a5786698c51b1c6e503f665ca34ce8d3272
Yuliia-Drahan <dragan1925@gmail.com>
2020-12-03 23:08:36 +0200

    Phones page tests

--- src/main/java/SmartPhonesPage.java
+++ src/main/java/SmartPhonesPage.java
@@ -1,5 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class SmartPhonesPage {
 
@@ -10,8 +13,59 @@ public class SmartPhonesPage {
     }
 
     private By heading = By.xpath("//div[contains(@class, 'page__title')]/h1");
+    private By okButton = By.xpath("//input[@class='listing__sidebar-range-submit button']");
+    private By samsungCheckBox = By.xpath("//input[@data-property-title='SAMSUNG']");
+    private By samsungCheckBoxChecked = By.xpath("//input[@data-property-title='SAMSUNG']/following-sibling::label");
+    private String samsungPhone = "//div[@data-product-title='%s']";
+    private By basketIcon = By.xpath("//i[@class='header-basket__icon icon-shop js-toggle-tooltip tooltip-link_open']");
+    private By basketTooltip = By.xpath("//div[@id='cartTooltip']");
+    private By checkOutButton = By.xpath("//a[text()='Оформить заказ']");
+    private By basketItems = By.xpath("//div[@class='header-tooltip__cards-item analytics-data main-product ']");
 
     public String getHeadingText(){
         return driver.findElement(heading).getText();
     }
+
+    public SmartPhonesPage chooseSamsungCheckBox(){
+        driver.findElement(samsungCheckBox).click();
+        return this;
+    }
+
+    public Boolean checkCheckBoxIsChecked(){
+        Boolean checked = driver.findElement(samsungCheckBoxChecked).isSelected();
+        return checked;
+    }
+
+    public SmartPhonesPage clickOkButton(){
+        driver.findElement(okButton).click();
+        return this;
+    }
+
+    public SmartPhonesPage clickBuyPhone(String phoneName){
+        driver.findElement(By.xpath(String.format(samsungPhone, phoneName)));
+        return this;
+    }
+
+    public SmartPhonesPage clickBasketIcon(){
+        driver.findElement(basketIcon).click();
+        return this;
+    }
+
+    public BasketPage clickCheckOutButton(){
+        driver.findElement(checkOutButton).click();
+        return new BasketPage(driver);
+    }
+
+    public Boolean checkBTooltipIsHere(){
+        Boolean displayed = driver.findElement(basketTooltip).isDisplayed();
+        return displayed;
+    }
+
+    public Integer checkItemsInBasket(){
+        List<WebElement> elements = driver.findElements(By.xpath("//div[@class='header-tooltip__cards-item analytics-data main-product ']"));
+        Integer numberOfItems = elements.size();
+        return numberOfItems;
+
+    }
+
 }
--- src/test/java/SmartPhonesPageTest.java
+++ src/test/java/SmartPhonesPageTest.java
@@ -1,2 +1,46 @@
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class SmartPhonesPageTest {
+    private WebDriver driver;
+    private SmartPhonesPage smartPhonesPage;
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.gecko.driver","C:\\Users\\user\\IdeaProjects\\FoxtrotTestProject\\drivers\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.foxtrot.com.ua/ru/shop/mobilnye_telefony_smartfon.html");
+        smartPhonesPage = new SmartPhonesPage(driver);
+    }
+
+    @Test
+    public void checkChockBox(){
+        smartPhonesPage.chooseSamsungCheckBox();
+        Boolean result = smartPhonesPage.checkCheckBoxIsChecked();
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void addItemsToBasket(){
+        smartPhonesPage.chooseSamsungCheckBox();
+        smartPhonesPage.clickOkButton();
+        smartPhonesPage.clickBuyPhone("Смартфон SAMSUNG Galaxy A51 4/64 Gb Dual Sim Black (SM-A515FZKUSEK)");
+        smartPhonesPage.clickBuyPhone("Смартфон SAMSUNG Galaxy S10 8/128Gb Duos Black (SM-G973FZKDSEK)");
+        smartPhonesPage.clickBuyPhone("Смартфон SAMSUNG Galaxy S20 8/128Gb Cosmic Gray (SM-G980FZADSEK)");
+        smartPhonesPage.clickBasketIcon();
+        Integer result = smartPhonesPage.checkItemsInBasket();
+        Assert.assertEquals(Integer.valueOf(3), result);
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+
 }
